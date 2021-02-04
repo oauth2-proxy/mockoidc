f0cf24a0f2085912e97fab3c063058db37da8126
Nick Meves <nick.meves@greenhouse.io>
2021-02-04 11:27:23 -0800

    Move userinfo JSON generation to User

--- user.go
+++ user.go
@@ -1,16 +1,18 @@
 package mockoidc
 
+import "encoding/json"
+
 // User represents a mock user that the server will grant Oauth tokens for.
 // Calls to the `authorization_endpoint` will pop any mock Users added to the
 // `UserQueue`. Otherwise `DefaultUser()` is returned.
 type User struct {
 	ID                string
-	Email             string `json:"email,omitempty"`
+	Email             string
 	EmailVerified     bool
-	PreferredUsername string   `json:"preferred_username,omitempty"`
-	Phone             string   `json:"phone,omitempty"`
-	Address           string   `json:"address,omitempty"`
-	Groups            []string `json:"groups,omitempty"`
+	PreferredUsername string
+	Phone             string
+	Address           string
+	Groups            []string
 }
 
 // DefaultUser returns a default User that is set in `authorization_endpoint`
@@ -25,6 +27,28 @@ func DefaultUser() *User {
 		Groups:            []string{"engineering", "design"},
 		EmailVerified:     true,
 	}
+}
+
+type userinfo struct {
+	Email             string   `json:"email,omitempty"`
+	PreferredUsername string   `json:"preferred_username,omitempty"`
+	Phone             string   `json:"phone,omitempty"`
+	Address           string   `json:"address,omitempty"`
+	Groups            []string `json:"groups,omitempty"`
+}
+
+func (u *User) userinfo(scopes []string) ([]byte, error) {
+	user := u.scopedClone(scopes)
+
+	ui := &userinfo{
+		Email:             user.Email,
+		PreferredUsername: user.PreferredUsername,
+		Phone:             user.Phone,
+		Address:           user.Address,
+		Groups:            user.Groups,
+	}
+
+	return json.Marshal(ui)
 }
 
 func (u *User) populateClaims(scopes []string, claims *idTokenClaims) {
